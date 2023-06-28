798d1d22a555d08afc1be13ff4dc5d7edfb9d383
Papon Mateevorakulkij <furby153@gmail.com>
2023-06-29 00:16:09 +0700

    Add onSearchChange

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import { robots } from "./robots";
 
-const App = () => {
-    return (
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
+    render(){
+       return (
         <div className="tc">
             <h1>Robofriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={this.state.robots}/>
         </div>  
-    );
+    ); 
+    }
 }
 
 export default App;
