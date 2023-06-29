6aba4f39fedc88c29c2ba739815e9b1728bb63be
Papon Mateevorakulkij <furby153@gmail.com>
2023-06-29 17:29:55 +0700

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
-import { robots } from "./robots";
+// import { robots } from "./robots";
 import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -27,13 +33,17 @@ class App extends React.Component {
                 inputRobot.email.toLowerCase().includes(this.state.searchfield.toLowerCase())
             );
         })
+        if (this.state.robots.length === 0){
+            return <h1 className='tc f1'>Loading</h1>
+        } else {
         return (
         <div className="tc">
             <h1 className="f1">Robofriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <CardList robots={filteredRobots}/>
         </div>  
-    ); 
+        );
+        } 
     }
 }
 
