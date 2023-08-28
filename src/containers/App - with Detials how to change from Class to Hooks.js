306c19d1fe3cd4d41c2d2824f8589dc96d5bafc8
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-28 19:26:02 +0700

    add file to see how to change from class to hooks

--- src/containers/App - with Detials how to change from Class to Hooks.js
+++ src/containers/App - with Detials how to change from Class to Hooks.js
@@ -6,10 +6,25 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 
 function App() {
-
+// Class App extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: '',
+    //     }
+    // }
+// When use Hooks instead of Class, Set state by doing as below:
     const [ robots, setRobots ] = useState([]);
     const [ searchfield, setSearchfield ] = useState('');
+    // const [ ชื่อstate, functionที่จะเปลี่ยนstate ] = useState(initialState);
 
+    // componentDidMount = async () => {
+    //     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    //     const users = await response.json();
+    //     this.setState({ robots: users });
+    // }
+// Change componentDidMount to useEffect because we doesn't use Class anymore.
     useEffect(() => {
         const fetchData = async () => {
           try {
@@ -23,10 +38,22 @@ function App() {
     
         fetchData();
       }, []); // Empty dependency array means this effect runs only once, similar to componentDidMount
+    
+
+    
 
     const onSearchChange = (event) => {
+        // this.setState({ searchfield: event.target.value }) 
+        // ไม่ได้ใช้ Class แล้ว ไม่มีการใช้ this.setState 
+        // และไม่ต้องทำแบบนี้ { ชื่อState: state ที่จะตั้ง } แล้ว
+        // โดยใช้ function ที่จะเปลี่ยน state แบบนี้แทน
+        // functionที่จะเปลี่ยนstate(ค่าที่จะให้เปลี่ยน);
         setSearchfield(event.target.value);
     }
+
+    // const { robots, searchfield } = this.state;
+    // พอไม่ได้ใช้ Class แล้ว ไม่มี render แล้ว ก็ไม่ต้องเรียก this.state แล้ว
+    // สามารถลบทิ้งได้
 
     const filteredRobots = robots.filter( inputRobot => {
         return (
@@ -49,7 +76,9 @@ function App() {
             </ErrorBoundary>
         </Scroll>
         </div>  
-    );    
+    );
+        
+    
 }
 
 export default App;
