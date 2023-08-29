dd99b3cda667d04e4a283b11243096f0790b9dad
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-29 19:53:49 +0700

    babel to devDependencies

--- src/actions.js
+++ src/actions.js
@@ -13,24 +13,22 @@ export const setSearchField = (text) => {
     }
 };
 
-export const requestRobots = () => (dispatch) => {
-    dispatch({ 
+export const requestRobots = () => async (dispatch) => {
+    dispatch({
         type: REQUEST_ROBOTS_PENDING
     });
-    fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(data => dispatch(
-                    { 
-                    type: REQUEST_ROBOTS_SUCCESS,
-                    payload: data
-                    }
-                )
-            )
-            .catch(error => dispatch(
-                    {
-                    type:REQUEST_ROBOTS_FAILED,
-                    payload: error
-                    }
-                )
-            )
-}
+
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        dispatch({
+            type: REQUEST_ROBOTS_SUCCESS,
+            payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: REQUEST_ROBOTS_FAILED,
+            payload: error
+        });
+    }
+};
